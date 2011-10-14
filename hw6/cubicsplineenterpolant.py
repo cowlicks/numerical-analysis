8066e9a9958ee606d5df85bad97572ebe70c4a3e
Blake <blake.a.griffith@gmail.com>
2011-10-13 23:48:34 -0500

    added comments

--- hw6/cubicsplineenterpolant.py
+++ hw6/cubicsplineenterpolant.py
@@ -1,2 +1,9 @@
 #! /usr/bin/python
 
+#the algorithm for the c_i's will be different for each boundary condition
+#get the eqn that is: c_i terms = a_i terms
+#the LHS forms a coeficiant matrix w/ the coeficiants of the c_i's
+#the RHS form a colum vector since, this gives a solvable system of eqn's
+#solve this w/ tridiagonal solution algorithm
+#use the c_i values to solve for the b_i's and d_i's
+
